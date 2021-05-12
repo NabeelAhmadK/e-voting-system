5c36fb0151038d6ba7a0dda3a0e82033f07f2417
Nabeel Ahmad Khan <nabeel.ahmed@odinesolutions.com>
2021-05-12 06:32:07 +0500

    Vote Page

--- src/pages/vote/index.jsx
+++ src/pages/vote/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import Tiles from '../../components/tiles'
+import { Row, Col } from 'antd';
+
 const Vote = () => {
+
+    const handleVoteClick = () => {
+        console.log('Hello');
+    }
+
     return (
-        <div>
-            Vote Component
+        <div >
+            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{ margin: '20px' }}>
+                <Col className="gutter-row" span={6} style={{ marginTop: '20px' }}>
+                    <Tiles
+                        partyName="Pakistan Tehreek-e-Insaaf"
+                        partyAbbreviation="PTI"
+                        partyLogo="smiley.png"
+                        handleClick={handleVoteClick} />
+                </Col >
+            </Row>
         </div>
     )
 }
-
 
 export default Vote
