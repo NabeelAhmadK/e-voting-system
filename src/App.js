e2ac19a51032021e99009ac90b981613a10da8a9
Nabeel Ahmad Khan <nabeel.ahmed@odinesolutions.com>
2021-05-12 06:32:19 +0500

    Styling fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Headers from './components/header';
 import Footers from './components/footer'
-import Verification from './pages/verification';
+// import Verification from './pages/verification';
+import Vote from './pages/vote';
 import './App.scss';
 
 const App = () => {
   return (
     <div>
       <Headers />
-      <Verification />
+      <Vote />
       <Footers />
     </div>
   );
